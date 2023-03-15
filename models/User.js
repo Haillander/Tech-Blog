c3d408bfd841b950d40fec125307cfa073cb43e6
Haillander <haillanderviana@gmail.com>
2023-03-15 13:36:13 +1100

    code added do models user.js

--- models/User.js
+++ models/User.js
@@ -1,35 +1,69 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+// hasing library
+const bcrypt = require('bcrypt');
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model which is the user table.
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-})
+User.init(
+    {
+      // define an id column
+      id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      // define a username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      // define a password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          // this means the password must be at least four characters long
+          len: [4]
+        }
+      }
+    },
+    {
+        //promise funciton async/await
+        //compare hash everytime a user is created and saved for later.
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+         },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+  );
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-})
-
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
-})
-
-Post.hasMany(Comment, {
-    foreignKey: 'post_id'
-})
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-})
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-})
 
 
-module.exports = {
-    User,
-    Post,
-    Comment
-};
+module.exports = User;
