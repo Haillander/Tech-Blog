8c753bc6c60d4c6c95724dc9da45d9c4d93ad33b
Haillander <haillanderviana@gmail.com>
2023-03-15 13:35:55 +1100

    made code clear to read

--- models/Post.js
+++ models/Post.js
@@ -1,52 +1,45 @@
-const {
-    Model,
-    DataTypes
-} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
 
+// create our Post model
+class Post extends Model {}
 
-class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
-}
-
-User.init({
-    id: {
+Post.init(
+    {
+      id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true
-    },
-    username: {
+        autoIncrement: true,
+        onDelete: 'CASCADE'
+      },
+      title: {
         type: DataTypes.STRING,
         allowNull: false
-    },
-    password: {
+      },
+      contents: {
         type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [4]
+        allowNull: false
+        // validate: {
+        //   isURL: true
+        // }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',//joing user Model on id column
+          // association is still required at index.js of models
+          key: 'id'
         }
-    }
-}, {
-    hooks: {
-        async beforeCreate(newUserData) {
-            newUserData.password = await bcrypt.hash(newUserData.password, 10);
-            return newUserData;
-        },
-        async beforeUpdate(updatedUserData) {
-            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-            return updatedUserData;
-        }
+      }
     },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
-})
+    {
+      sequelize,
+      // what each of these metadata means?
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
 
-
-module.exports = User;
+  module.exports = Post;
