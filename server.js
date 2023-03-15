ac55d2577d9e461d0952f8508612f31c07ab720f
Haillander <haillanderviana@gmail.com>
2023-03-15 18:46:16 +1100

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
